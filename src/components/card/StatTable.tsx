7a0dcb4e1d64e88379a186374aebac02a59d71f6
LoveLive <lovelivefam@gmail.com>
2020-01-31 18:27:51 +0900

    Improve card stat table

--- src/components/card/StatTable.tsx
+++ src/components/card/StatTable.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import { PARAMETER } from '@/data/cardParameter';
-import { FULL_CARD_LIST } from '@/data/cardList';
 
 const StyledTable = styled.table`
   border: 1px solid black;
@@ -9,10 +8,14 @@ const StyledTable = styled.table`
 
   td, th {
     text-align: center;
-    padding: 4px;
+    padding: 8px;
     border: 1px solid black;
     white-space: nowrap;
   }
+`;
+
+const SmallFont = styled.td`
+  font-size: 0.6em;
 `;
 
 interface OwnProps {
@@ -32,117 +35,42 @@ const StatTable: React.FC<StatTableProps> = ({
         <tr>
           <td>한돌</td>
           {uncapArray.map((uncap) => (
-            <>
+            <React.Fragment key={uncap}>
               <td>{uncap}</td>
               {uncap !== maxUncap && <td>-</td>}
-            </>
+            </React.Fragment>
           ))}
         </tr>
-        <tr>
-          <td>어필</td>
-          {uncapArray.map((uncap) => {
-            const applExist = parameter[uncap].appl !== 0;
-            return (
-              <>
-                <td>{applExist ? parameter[uncap].appl : '?'}</td>
-                {uncap !== maxUncap && (
-                  <td>{applExist ? `+${parameter[uncap + 1].appl - parameter[uncap].appl}` : '?'}</td>
-                )}
-              </>
-            );
-          })}
-        </tr>
-        <tr>
-          <td>동일 한돌 내 어필 순위</td>
-          {uncapArray.map((uncap) => {
-            if (parameter[uncap].appl !== 0) {
-              const sameUncapList = FULL_CARD_LIST.filter((card) => card.uncap === uncap);
-              const rank = sameUncapList.sort((a, b) => b.appl - a.appl).findIndex((card) => card.id === id) + 1;
-              return (
-                <>
-                  <td>{`#${rank}/${sameUncapList.length}`}</td>
-                  {uncap !== maxUncap && <td />}
-                </>
-              );
-            }
-            return (
-              <>
-                <td>?</td>
-                {uncap !== maxUncap && <td />}
-              </>
-            );
-          })}
-        </tr>
-        <tr>
-          <td>스태미너</td>
-          {uncapArray.map((uncap) => {
-            const stamExist = parameter[uncap].stam !== 0;
-            return (
-              <>
-                <td>{stamExist ? parameter[uncap].stam : '?'}</td>
-                {uncap !== maxUncap && (
-                  <td>{stamExist ? `+${parameter[uncap + 1].stam - parameter[uncap].stam}` : '?'}</td>
-                )}
-              </>
-            );
-          })}
-        </tr>
-        <tr>
-          <td>동일 한돌 내 스태미너 순위</td>
-          {uncapArray.map((uncap) => {
-            if (parameter[uncap].stam !== 0) {
-              const sameUncapList = FULL_CARD_LIST.filter((card) => card.uncap === uncap);
-              const rank = sameUncapList.sort((a, b) => b.stam - a.stam).findIndex((card) => card.id === id) + 1;
-              return (
-                <>
-                  <td>{`#${rank}/${sameUncapList.length}`}</td>
-                  {uncap !== maxUncap && <td />}
-                </>
-              );
-            }
-            return (
-              <>
-                <td>?</td>
-                {uncap !== maxUncap && <td />}
-              </>
-            );
-          })}
-        </tr>
-        <tr>
-          <td>테크닉</td>
-          {uncapArray.map((uncap) => {
-            const techExist = parameter[uncap].tech !== 0;
-            return (
-              <>
-                <td>{techExist ? parameter[uncap].tech : '?'}</td>
-                {uncap !== maxUncap && (
-                  <td>{techExist ? `+${parameter[uncap + 1].tech - parameter[uncap].tech}` : '?'}</td>
-                )}
-              </>
-            );
-          })}
-        </tr>
-        <tr>
-          <td>동일 한돌 내 테크닉 순위</td>
-          {uncapArray.map((uncap) => {
-            if (parameter[uncap].tech !== 0) {
-              const sameUncapList = FULL_CARD_LIST.filter((card) => card.uncap === uncap);
-              const rank = sameUncapList.sort((a, b) => b.tech - a.tech).findIndex((card) => card.id === id) + 1;
-              return (
-                <>
-                  <td>{`#${rank}/${sameUncapList.length}`}</td>
-                  {uncap !== maxUncap && <td />}
-                </>
-              );
-            }
-            return (
-              <>
-                <td>?</td>
-                {uncap !== maxUncap && <td />}
-              </>
-            );
-          })}
-        </tr>
+        {(['appl', 'stam', 'tech'] as const).map((stat) => {
+          const statName = {
+            appl: '어필',
+            stam: '스태미너',
+            tech: '테크닉',
+          };
+          const statColor = {
+            appl: '#E51C60',
+            stam: '#078A21',
+            tech: '#A56A0A',
+          };
+          return (
+            <tr style={{ color: statColor[stat] }} key={stat}>
+              <td>{statName[stat]}</td>
+              {uncapArray.map((uncap) => {
+                const statExist = parameter[uncap][stat] !== 0;
+                return (
+                  <React.Fragment key={uncap}>
+                    <td>{statExist ? parameter[uncap][stat] : '?'}</td>
+                    {uncap !== maxUncap && (
+                      <SmallFont>
+                        {statExist ? `+${parameter[uncap + 1][stat] - parameter[uncap][stat]}` : '?' }
+                      </SmallFont>
+                    )}
+                  </React.Fragment>
+                );
+              })}
+            </tr>
+          );
+        })}
       </tbody>
     </StyledTable>
   );
