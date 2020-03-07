b11899066c5a7428d785771363fe9dd1fe28e146
LoveLive <lovelivefam@gmail.com>
2020-03-08 03:09:23 +0900

    Add help message in card comparator

--- src/components/card-comparator/CardComparator.tsx
+++ src/components/card-comparator/CardComparator.tsx
@@ -16,10 +16,31 @@ const RowWrapper = styled(FixedWrapper)`
   padding-top: 4px;
   padding-bottom: 4px;
 `;
+const CenterP = styled.p`
+  text-align: center;
+  line-height: 1.6em;
+`;
 
 const CardComparator: React.FC = () => (
   <>
     <VerticalFlex>
+      <RowWrapper>
+        <CenterP>
+          여기서 계산되는 스탯은 아래 식을 통해 계산됩니다:
+          <br />
+          (카드 기본 스탯) * (인연 버프) * (기본 스탯 증감) * (스탯 증감) * (동속성 효과)
+          <br />
+          카드 자체의 특기, 개성(패시브), 타입 등은 계산에 영향을 미치지 않습니다! 주의해주세요.
+          <br />
+          <strong>
+            크리티컬 관련 추정은&nbsp;
+            <a href="/cards">
+              카드 목록
+            </a>
+            에 있는 가정을 그대로 적용한 수치입니다.
+          </strong>
+        </CenterP>
+      </RowWrapper>
       <RowWrapper style={{ border: '1px solid black' }}>
         <Settings />
       </RowWrapper>
