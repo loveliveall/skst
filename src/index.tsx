c35889bc02fae946eabb2ebc09352a28327b2108
LoveLive <lovelivefam@gmail.com>
2020-06-18 00:21:45 +0900

    Add GA to site

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import Analytics from 'react-router-ga';
 
 import App from '@/components/App';
 import { store } from '@/store';
@@ -11,7 +12,9 @@ import '@/data/_validator'; // Validating data
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <Analytics id="UA-142042916-2">
+        <App />
+      </Analytics>
     </BrowserRouter>
   </Provider>,
   document.getElementById('app'),
