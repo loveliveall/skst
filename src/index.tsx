c26c5488b776b6bd711378ea3b4a3629831da2d6
LoveLive <lovelivefam@gmail.com>
2020-11-24 22:14:37 +0900

    Remove everything before we do fresh restart

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import Analytics from 'react-router-ga';
 
 import App from '@/components/App';
-import { store } from '@/store';
-
-import '@/data/_validator'; // Validating data
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Analytics id="UA-142042916-2">
-        <App />
-      </Analytics>
-    </BrowserRouter>
-  </Provider>,
+  <BrowserRouter>
+    <Analytics id="UA-142042916-2">
+      <App />
+    </Analytics>
+  </BrowserRouter>,
   document.getElementById('app'),
 );
--- src/store/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { Middleware } from 'redux';
-
-export const loggerMiddleware: Middleware = ({ getState }) => (next) => (action) => {
-  console.debug('[Action]', action);
-  const returnValue = next(action);
-  console.debug('[Applied State]', getState());
-  return returnValue;
-};
